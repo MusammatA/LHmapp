11e87bbcd7d88c8c380f9e88c81406b7e121d19e
MusammatA <mea2222@columbia.edu>
2026-04-20 11:47:07 -0400

    Fix intro overlay teardown

--- js/app.js
+++ js/app.js
@@ -16,6 +16,25 @@
 
   function createIntroController(elements, mapController) {
     let hasStarted = false;
+    let hasCompleted = false;
+    let fallbackTimerId = null;
+
+    function completeReveal() {
+      if (hasCompleted) {
+        return;
+      }
+
+      hasCompleted = true;
+
+      if (fallbackTimerId) {
+        globalScope.clearTimeout(fallbackTimerId);
+      }
+
+      elements.pageElement.classList.remove("is-intro-active", "is-intro-leaving");
+      elements.introElement.hidden = true;
+      elements.introElement.setAttribute("aria-hidden", "true");
+      mapController.setInteractivity(true);
+    }
 
     function revealMap() {
       if (hasStarted) {
@@ -23,13 +42,20 @@
       }
 
       hasStarted = true;
+      elements.introElement.setAttribute("aria-hidden", "true");
       elements.pageElement.classList.add("is-intro-leaving");
 
-      globalScope.setTimeout(() => {
-        elements.pageElement.classList.remove("is-intro-active", "is-intro-leaving");
-        elements.introElement.hidden = true;
-        mapController.setInteractivity(true);
-      }, INTRO_TRANSITION_MS);
+      fallbackTimerId = globalScope.setTimeout(completeReveal, INTRO_TRANSITION_MS + 120);
+    }
+
+    function handleIntroTransitionEnd(event) {
+      if (
+        event.target === elements.introElement &&
+        event.propertyName === "opacity" &&
+        hasStarted
+      ) {
+        completeReveal();
+      }
     }
 
     if (!elements.introElement || !elements.pageElement) {
@@ -37,7 +63,9 @@
       return;
     }
 
+    elements.introElement.setAttribute("aria-hidden", "false");
     elements.introElement.addEventListener("click", revealMap);
+    elements.introElement.addEventListener("transitionend", handleIntroTransitionEnd);
   }
 
   function bootMap() {
